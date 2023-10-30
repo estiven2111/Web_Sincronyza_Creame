c3301b8af9dc16c9c5bc1270e226e817716984cb
estiven2111 <estiven.a.martinez@gmail.com>
2023-10-30 14:21:03 -0500

    input

--- src/componets/gastos/gastos.jsx
+++ src/componets/gastos/gastos.jsx
@@ -669,7 +669,7 @@ const Gastos = () => {
                 </div>
               </div>
               {/* NOMBRE */}
-              {/* <div className="flex items-center justify-center col-span-1">
+              <div className="flex items-center justify-center col-span-1">
                 <div
                   className="relative mb-3 w-full  "
                   data-te-input-wrapper-init
@@ -689,23 +689,7 @@ const Gastos = () => {
                     Nombre
                   </label>
                 </div>
-              </div> */}
-              <div className="relative mb-3 w-full" data-te-input-wrapper-init>
-  <input
-    value={responsedata.nombre}
-    name="nombre"
-    onChange={handleOnChange}
-    type="text"
-    className="peer block min-h-[auto] w-full rounded border-0 bg-blue-300/50 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
-    id="Nombre"
-  />
-  <label
-    htmlFor="Nombre"
-    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none"
-  >
-    Nombre
-  </label>
-</div>
+              </div>
               {/* VALOR PAGADO */}
               <div className="flex items-center justify-center col-span-1 ">
                 <div

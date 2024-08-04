5160eab307635df5c49917cec1c39ada77e3cc4a
estiven2111 <estiven.a.martinez@gmail.com>
2024-08-04 16:09:12 -0500

    pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,38 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'app_creame',
+//       script: 'app.js', // Archivo de entrada de tu aplicación
+//       env: {
+//         NODE_ENV: 'development'
+//       },
+//       env_production_creame: {
+//         NODE_ENV: 'production'
+//       }
+//     },
+//     {
+//       name: 'another_app',
+//       script: 'another_app.js',
+//       env: {
+//         NODE_ENV: 'development'
+//       },
+//       env_production_creame: {
+//         NODE_ENV: 'production'
+//       }
+//     }
+//   ]
+// };
+
 module.exports = {
   apps: [
     {
-      name: 'app_creame',
-      script: 'app.js', // Archivo de entrada de tu aplicación
+      name: 'react-app',
+      script: 'node_modules/.bin/http-server',
+      args: 'build -p 3400',
       env: {
         NODE_ENV: 'development'
       },
-      env_production_creame: {
-        NODE_ENV: 'production'
-      }
-    },
-    {
-      name: 'another_app',
-      script: 'another_app.js',
-      env: {
-        NODE_ENV: 'development'
-      },
-      env_production_creame: {
+      env_production: {
         NODE_ENV: 'production'
       }
     }
